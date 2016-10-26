79f655229b239f2f077fb6fa3f2e63fdaf18e757
shuheiktgw <shuhei.kitagawa@gmail.com>
2016-10-26 11:06:51 +0900

    done fefactoring so far

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -1,7 +1,7 @@
 var React = require('react');
 
-var WeatherMessage = (props) => {
-  var {temp, location} = props;
+var WeatherMessage = ({temp, location}) => {
+  // var {temp, location} = props; the same with the above...
   retunr(
     <h3>{location}'s temperature is {temp}</h3>
   );
