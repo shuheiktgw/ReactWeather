6874ccd3b158cbb01db9fde38fde9a1d607dde9d
shuheiktgw <shuhei.kitagawa@gmail.com>
2016-10-27 09:42:01 +0900

    add errro modal

--- app/api/openWeatherMap.jsx
+++ app/api/openWeatherMap.jsx
@@ -1,6 +1,6 @@
 var axios = require('axios');
 
-const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/find?units=metric&appid=9826f0a7cf13bc549c0b6dc6bd9d111d';
+const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/find?units=metric&appid=c60b7208bb92efdc1079c99a9a4f46b4';
 
 module.exports = {
   getTemp: function (location) {
--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -2,7 +2,7 @@ var React = require('react');
 
 var WeatherMessage = ({temp, location}) => {
   // var {temp, location} = props; the same with the above...
-  retunr(
+  return(
     <h3 className="text-center">{location}'s temperature is {temp}</h3>
   );
 };
